c219cceb00b7994751c19e228e9c3692193e87a0
yuzuki24 <yuzuki.football@gmail.com>
2022-05-10 15:27:43 +0900

    装飾

--- vamos/views.py
+++ vamos/views.py
@@ -10,19 +10,19 @@ from .models import Message, Friend, Group, Good
 from .forms import GroupCheckForm, GroupSelectForm,\
     FriendsForm, CreateGroupForm, PostForm
 
-# indexのビュー関数
+# index部分
 
 
 @login_required(login_url='/admin/login/')
 def index(request, page=1):
-    # publicのuserを取得
+ # publicのuserを取得
     (public_user, public_group) = get_public()
 
-    # POST送信時の処理
+# POST送信時
     if request.method == 'POST':
 
-        # Groupsのチェックを更新した時の処理
-        # フォームの用意
+        # Groupsのチェックを更新した時
+        # フォーム用意
         checkform = GroupCheckForm(request.user, request.POST)
         # チェックされたGroup名をリストにまとめる
         glist = []
@@ -32,9 +32,9 @@ def index(request, page=1):
         messages = get_your_group_message(request.user,
                                           glist, page)
 
-    # GETアクセス時の処理
+ # GETアクセス時
     else:
-        # フォームの用意
+        # フォーム用意
         checkform = GroupCheckForm(request.user)
         # Groupのリストを取得
         gps = Group.objects.filter(owner=request.user)
@@ -43,8 +43,7 @@ def index(request, page=1):
             glist.append(item.title)
         # メッセージの取得
         messages = get_your_group_message(request.user, glist, page)
-
-    # 共通処理
+# 共通処理
     params = {
         'login_user': request.user,
         'contents': messages,
@@ -58,7 +57,7 @@ def groups(request):
     # 自分が登録したFriendを取得
     friends = Friend.objects.filter(owner=request.user)
 
-    # POST送信時の処理
+ # POST送信時
     if request.method == 'POST':
 
         # Groupsメニュー選択肢の処理
@@ -76,12 +75,12 @@ def groups(request):
             vlist = []
             for item in fds:
                 vlist.append(item.user.username)
-            # フォームの用意
+        # フォームの用意
             groupsform = GroupSelectForm(request.user, request.POST)
             friendsform = FriendsForm(request.user,
                                       friends=friends, vals=vlist)
 
-        # Friendsのチェック更新時の処理
+        # Friendsのチェック更新時
         if request.POST['mode'] == '__friends_form__':
             # 選択したGroupの取得
             sel_group = request.POST['group']
@@ -89,12 +88,13 @@ def groups(request):
             print(group_obj)
             # チェックしたFriendsを取得
             sel_fds = request.POST.getlist('friends')
-            # FriendsのUserを取得
+
+        # FriendsのUserを取得
             sel_users = User.objects.filter(username__in=sel_fds)
-            # Userのリストに含まれるユーザーが登録したFriendを取得
+        # Userのリストに含まれるユーザーが登録したFriendを取得
             fds = Friend.objects.filter(owner=request.user) \
                 .filter(user__in=sel_users)
-            # すべてのFriendにGroupを設定し保存する
+            # すべてのFriendにGroupを設定し保存
             vlist = []
             for item in fds:
                 item.group = group_obj
@@ -109,7 +109,7 @@ def groups(request):
             friendsform = FriendsForm(request.user,
                                       friends=friends, vals=vlist)
 
-    # GETアクセス時の処理
+ # GETアクセス時の処理
     else:
         # フォームの用意
         groupsform = GroupSelectForm(request.user)
@@ -117,7 +117,7 @@ def groups(request):
                                   vals=[])
         sel_group = '-'
 
-    # 共通処理
+# 共通処理
     createform = CreateGroupForm()
     params = {
         'login_user': request.user,
@@ -128,15 +128,15 @@ def groups(request):
     }
     return render(request, 'vamos/groups.html', params)
 
-# Friendの追加処理
 
+# Friendの追加処理
 
 @login_required(login_url='/admin/login/')
 def add(request):
     # 追加するUserを取得
     add_name = request.GET['name']
     add_user = User.objects.filter(username=add_name).first()
-    # Userが本人だった場合の処理
+    # Userが本人だった場合
     if add_user == request.user:
         messages.info(request, "自分自身をFriendに追加することは\
                 できません。")
@@ -201,19 +201,19 @@ def post(request):
         messages.success(request, '新しいメッセージを投稿しました！')
         return redirect(to='/vamos')
 
-    # GETアクセス時の処理
+ # GETアクセス時の処理
     else:
         form = PostForm(request.user)
 
-    # 共通処理
+ # 共通処理
     params = {
         'login_user': request.user,
         'form': form,
     }
     return render(request, 'vamos/post.html', params)
 
-# 投稿をシェアする
 
+# 投稿をシェア
 
 @login_required(login_url='/admin/login/')
 def share(request, share_id):
@@ -230,7 +230,7 @@ def share(request, share_id):
             .filter(title=gr_name).first()
         if group == None:
             (pub_user, group) = get_public()
-        # メッセージを作成し、設定をして保存
+        # メッセージを作成、設定をして保存
         msg = Message()
         msg.owner = request.user
         msg.group = group
@@ -241,7 +241,7 @@ def share(request, share_id):
         share_msg.share_count += 1
         share_msg.save()
         # メッセージを設定
-        messages.success(request, 'メッセージをシェアしました！')
+        messages.success(request, 'シェアしました！')
         return redirect(to='/vamos')
 
     # 共通処理
@@ -253,22 +253,22 @@ def share(request, share_id):
     }
     return render(request, 'vamos/share.html', params)
 
-# goodボタンの処理
 
+# goodボタンの処理
 
 @login_required(login_url='/admin/login/')
 def good(request, good_id):
-    # goodするMessageを取得
+    # いいねするMessageを取得
     good_msg = Message.objects.get(id=good_id)
-    # 自分がメッセージにGoodした数を調べる
+    # 自分がメッセージにいいねした数を調べる
     is_good = Good.objects.filter(owner=request.user) \
         .filter(message=good_msg).count()
-    # ゼロより大きければ既にgood済み
+    # ゼロより大きければ既にいいね済み
     if is_good > 0:
-        messages.success(request, '既にメッセージにはGoodしています。')
+        messages.success(request, 'すでにメッセージにはGoodしています。')
         return redirect(to='/vamos')
 
-    # Messageのgood_countを１増やす
+    # Messageのいいねを１増やす
     good_msg.good_count += 1
     good_msg.save()
     # Goodを作成し、設定して保存
@@ -284,7 +284,7 @@ def good(request, good_id):
 
 
 def get_your_group_message(owner, glist, page):
-    page_num = 10  # ページあたりの表示数
+    page_num = 10  # ページの表示数
     # publicの取得
     (public_user, public_group) = get_public()
     # チェックされたGroupの取得
@@ -306,7 +306,7 @@ def get_your_group_message(owner, glist, page):
     # groupがgroupsに含まれるか、me_groupsに含まれるMessageの取得
     messages = Message.objects.filter(Q(group__in=groups)
                                       | Q(group__in=me_groups))
-    # ページネーションで指定ページを取得
+    # ページネーションでページ取得
     page_item = Paginator(messages, page_num)
     return page_item.get_page(page)
 
@@ -317,6 +317,3 @@ def get_public():
     public_user = User.objects.filter(username='public').first()
     public_group = Group.objects.filter(owner=public_user).first()
     return (public_user, public_group)
-
-
-# Create your views here.
